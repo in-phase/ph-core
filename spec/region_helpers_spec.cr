4e48105bce8e06a63aabcc9512729ae94dfaaac0
emgineering <emily.jeanette.love@gmail.com>
2021-04-29 14:21:55 -0600

    a few more tests for region_helpers

--- spec/region_helpers_spec.cr
+++ spec/region_helpers_spec.cr
@@ -115,18 +115,39 @@ describe Lattice::RegionHelpers do
         # see SteppedRange.new
     end
     pending ".canonicalize_region" do
+        it "infers implicit trailing ranges" do
+            
+        end
       
     end
-    pending ".measure_canonical_region" do
-        it "measures correctly" do
-            # region = canonicalize_region([])
+    describe ".measure_canonical_region" do
+        # TODO: check for robustness
+        regions = [
+            canonicalize_region([1..4..20, 7...2, ..], [30, 10, 10]),
+            canonicalize_region([1..1, 5..5, 3..3], [30,10,10])
+        ]
+        it "generates a shape of same dimensionality as the region" do 
+            regions.each do |region|
+                measure_canonical_region(region).size.should eq region.size
+            end
+        end
+        it "measures correctly" do 
+            measure_canonical_region(regions[0]).should eq [5, 5, 10]
+            measure_canonical_region(regions[1]).should eq [1, 1, 1]
         end
     end
-    pending ".measure_region" do
+    describe ".measure_region" do 
+        # TODO: check for robustness
+        regions = [[1..4..20, 7...2], [1..1, 5..5, 3..3]]
+        shape = [30, 10, 10]
+        it "generates a shape of the same dimensionality as the input shape" do 
+            regions.each do |region|
+                measure_region(region, shape).size.should eq shape.size
+            end
+        end
         # see canonicalize region, measure_canonical_region
     end
     describe "SteppedRange" do
-
         describe ".new" do
             pending "computes size" do 
             end
