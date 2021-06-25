55f91f887e0deb8c3d9083c011f63d380f4c8fb0
Emily Love <emily.jeanette.love@gmail.com>
2021-06-24 19:36:58 -0700

    generalize coorditerators more

--- src/iterators/coord_iterator.cr
+++ src/iterators/coord_iterator.cr
@@ -1,29 +1,12 @@
 require "big"
 
+
 module Lattice
-  abstract class CoordIterator(T)
-    include Iterator(Coord)
 
-    getter coord : Array(T)
+  # Strictly finite regions
+  abstract class CoordIterator(T) < GeneralCoordIterator(T)
 
-    @first : Array(T)
     @last : Array(T)
-    @step : Array(T)
-
-    # Informs the iterator to not update the coord, i.e. if the iterator 
-    # is empty or when returning the first item
-    @hold_coord : Bool = true
-    @empty : Bool = false
-    getter size : BigInt
-
-    abstract def advance_coord
-
-    # Set up any incrementing variables (such as @coord) here prior to iteration.
-    def reset : self
-      @coord = @first.dup
-      @hold_coord = true 
-      self
-    end
 
     def self.from_canonical(first, last, step, size = nil)
       if !size
@@ -31,6 +14,8 @@ module Lattice
       end
       self.new(first, last, step, size)
     end
+
+
 
     def initialize(shape : Indexable(T), region : CanonicalRegion? = nil, reverse : Bool = false)
       # TODO: When crystal 1.1.0 comes out, move the @coord initializer up to `getter coord = [] of T`.
@@ -48,21 +33,6 @@ module Lattice
       @coord = [] of T
       @empty = @size == 0
       reset
-    end
-
-    def next : (Array(T) | Stop)
-      if @hold_coord
-        return stop if @empty 
-        # if the iterator is nonempty, we only hold for the first coord
-        @hold_coord = false 
-        return @coord
-      end
-      advance_coord
-    end
-
-    # TODO: constrain, figure out what +1 means and if it should depend on step, generally test heavily
-    def unsafe_skip(axis, amount) : Nil
-      @coord[axis] += amount + 1
     end
 
     def reverse!
@@ -110,6 +80,52 @@ module Lattice
         end
       end
       {first, last, step, size}
+    end
+  end
+
+  abstract class GeneralCoordIterator(T)
+    include Iterator(Coord)
+
+    getter coord : Array(T)
+    @first : Array(T)
+    @step : Array(T)
+
+
+    # def initialize(region):
+    #   canonicalize_stuff
+      # - negative to positive 
+      # - inferred endpoints: forgive  (throw error on inferred start)
+      # - -------------exclusivity <=  ONLY HANDLE THIS
+
+
+    # Informs the iterator to not update the coord, i.e. if the iterator 
+    # is empty or when returning the first item
+    @hold_coord : Bool = true
+    @empty : Bool = false
+    getter size : BigInt
+
+    abstract def advance_coord
+
+    # Set up any incrementing variables (such as @coord) here prior to iteration.
+    def reset : self
+      @coord = @first.dup
+      @hold_coord = true 
+      self
+    end
+
+    def next : (Array(T) | Stop)
+      if @hold_coord
+        return stop if @empty 
+        # if the iterator is nonempty, we only hold for the first coord
+        @hold_coord = false 
+        return @coord
+      end
+      advance_coord
+    end
+
+    # TODO: constrain, figure out what +1 means and if it should depend on step, generally test heavily
+    def unsafe_skip(axis, amount) : Nil
+      @coord[axis] += amount + 1
     end
   end
 end
