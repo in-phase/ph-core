32e8fbe898f40aee521ac19c03889dd2031dc29a
Seth Hinz <sethhinz@me.com>
2021-08-03 17:09:00 -0700

    bugfixes and a reparameterization of ChunkIterators to mitigate Crystal#11058

--- src/iterators/chunk_iterator.cr
+++ src/iterators/chunk_iterator.cr
@@ -1,14 +1,14 @@
 require "./chunk_region_iterator.cr"
 
 module Phase
-  class ChunkIterator(E, I)
-    include Iterator(MultiIndexable(E))
+  class ChunkIterator(C, E, I)
+    include Iterator(C)
 
     alias FB = RegionIterator::FringeBehaviour
 
-    @chunk_and_region_iterator : ChunkAndRegionIterator(E, I)
+    @chunk_and_region_iterator : ChunkAndRegionIterator(C, E, I)
 
-    def initialize(@chunk_and_region_iterator : ChunkAndRegionIterator(E, I))
+    def initialize(@chunk_and_region_iterator : ChunkAndRegionIterator(C, E, I))
     end
 
     def self.new(src, chunk_shape, strides = nil, degeneracy = nil,
@@ -21,14 +21,8 @@ module Phase
       new(ChunkAndRegionIterator.new(src, chunk_shape, strides, degeneracy, fringe_behaviour))
     end
 
-    def next : MultiIndexable(E) | Stop
-      case val = @chunk_and_region_iterator.next_value
-      when MultiIndexable(E)
-        # HACK: mitigates virtual type issues https://forum.crystal-lang.org/t/virtual-types-causing-unexpected-behaviour/3584
-        val.unsafe_as(MultiIndexable(E))
-      else
-        stop
-      end
+    def next : C | Stop
+      @chunk_and_region_iterator.next_value
     end
 
     def reset
--- src/iterators/chunk_region_iterator.cr
+++ src/iterators/chunk_region_iterator.cr
@@ -1,42 +1,44 @@
 require "./region_iterator"
 
 module Phase
-  class ChunkAndRegionIterator(E, I)
-    # VerboseChunkIterator ?
-    # TODO: IndexRegion(Int32) is a needless restriction - find a way to
-    # allow generic coordiate types in the future
-    include Iterator(Tuple(MultiIndexable(E), IndexRegion(I)))
+  class ChunkAndRegionIterator(C, E, I)
+    include Iterator(Tuple(C, IndexRegion(I)))
+
     alias FB = RegionIterator::FringeBehaviour
 
     @region_iter : RegionIterator(I)
     @src : MultiIndexable(E)
 
-    # TODO: change from classes to instances of iter
-    def self.new(src, chunk_shape, strides = nil, degeneracy = nil,
+    def self.new(src : MultiIndexable, chunk_shape : Shape, strides : Coord? = nil, degeneracy = nil,
                  fringe_behaviour : FB = FB::DISCARD, &block)
-      new(src, RegionIterator.new(src.shape, chunk_shape, strides, degeneracy, fringe_behaviour) { |region| yield region })
+      region_iter = RegionIterator.new(src.shape, chunk_shape, strides, degeneracy, fringe_behaviour) { |region| yield region }
+      from(src, region_iter)
     end
 
-    def self.new(src, chunk_shape, strides = nil, degeneracy = nil,
+    def self.new(src : MultiIndexable, chunk_shape : Shape, strides : Coord? = nil, degeneracy = nil,
                  fringe_behaviour : FB = FB::DISCARD)
-      new(src, RegionIterator.new(src.shape, chunk_shape, strides, degeneracy, fringe_behaviour))
+      region_iter = RegionIterator.new(src.shape, chunk_shape, strides, degeneracy, fringe_behaviour)
+      from(src, region_iter)
+    end
+
+    protected def self.from(src : MultiIndexable(E), region_iter : RegionIterator(I))
+      ChunkAndRegionIterator(typeof(src.unsafe_fetch_chunk(region_iter.unsafe_next)), E, I).new(src, region_iter)
     end
 
     def initialize(@src : MultiIndexable(E), @region_iter : RegionIterator(I))
     end
 
-    def next : Stop | Tuple(MultiIndexable(E), IndexRegion(I))
+    def next : Stop | Tuple(C, IndexRegion(I))
       case region = @region_iter.next
       in IndexRegion
-        chunk = @src.unsafe_fetch_chunk(region, drop: true)
-        # HACK: mitigates virtual type issues https://forum.crystal-lang.org/t/virtual-types-causing-unexpected-behaviour/3584
-        {chunk.unsafe_as(MultiIndexable(E)), region}
+        chunk = @src.unsafe_fetch_chunk(region, drop: true).as(C)
+        {chunk, region}
       in Stop
         stop
       end
     end
 
-    def next_value : Stop | MultiIndexable(E)
+    def next_value : Stop | C
       case maybe_tuple = self.next
       in Tuple
         maybe_tuple[0]
