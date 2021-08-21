1c554e5262a5d4f81b884811dae20746e15a0901
Emily Love <emily.jeanette.love@gmail.com>
2021-08-21 14:37:00 -0700

    clean up junk

--- benchmark_test/benchmark.cr
+++
@@ -1,9 +0,0 @@
-require "./*"
-
-puts "running a benchmark"
-
-# benchmark_helper.cr
-def benchmark(title, &block)
-  # run the block
-  # save the output to an IO
-end
--- test_benchmark/src/main.cr
+++
@@ -1,9 +0,0 @@
-def square(n)
-  sum = 0
-  n.times do
-    n.times do
-      sum += 1
-    end
-  end
-  sum
-end
